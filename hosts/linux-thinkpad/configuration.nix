e7d1f7e139ac4158fdad81f066f55b58d35704a7
melody <melody@unplug.red>
2026-06-26 19:03:27 +0300

    better pipewire support

--- hosts/linux-thinkpad/configuration.nix
+++ hosts/linux-thinkpad/configuration.nix
@@ -281,8 +281,11 @@ Exec=sway'';
 
 	#services.printing.enable = true;
 
+	security.rtkit.enable = true;
 	services.pipewire = {
 		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
 		pulse.enable = true;
 	};
 
