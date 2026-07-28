8bf1c155780a24e7a74a6a512b350757c4519021
melody <melody@unplug.red>
2026-07-28 22:10:41 +0300

    updated packages

---
+++ rebuild_nix.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo nixos-rebuild switch --flake /home/mel/repos/dotfiles#linux-thinkpad --impure
--- update_nix.sh
+++ update_nix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+sudo nix-collect-garbage -d
+nix-collect-garbage -d
+nix-store --optimise
+sudo nix flake update --flake flake.nix
 sudo nixos-rebuild switch --flake /home/mel/repos/dotfiles#linux-thinkpad --impure
